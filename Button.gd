697aed0d88bd23f0aa34233fbebdf4a03dfca999
ObfuscatedOwl <mathteare@gmail.com>
2021-01-30 17:19:45 +0000

    Adding ocean control

---
+++ Button.gd
@@ -0,0 +1,20 @@
+extends Spatial
+
+var activated = false
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+#func _process(delta):
+#	pass
+
+
+func _on_Area_input_event(camera, event, click_position, click_normal, shape_idx):
+	if event is InputEventMouseButton:
+		if event.button_index == BUTTON_LEFT and event.pressed:
+			activated = not activated
+			print("activated" + str(activated))
+			$"/root/global".ocean.modifyHeight(activated)
--- OceanController.gd
+++ OceanController.gd
@@ -1,16 +1,28 @@
-extends Sprite3D
+extends Spatial
 
 
 # Declare member variables here. Examples:
 # var a = 2
 # var b = "text"
-var height = 3
+var desiredHeight = -1
+var height = 0
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	pass # Replace with function body.
+	$"/root/global".register_player(self)
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 #func _process(delta):
 #	pass
+
+func _physics_process(delta):
+	height = lerp(height, desiredHeight, 0.01)
+	translation.y = height
+	
+func modifyHeight(raise):
+	print("hello")
+	if raise:
+		desiredHeight += 1
+	else:
+		desiredHeight -= 1
